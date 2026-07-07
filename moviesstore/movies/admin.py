76f7285361418c840ffeadb0475e208d3ccbe0b0
Mohamed Kone <konemohamedflo.com>
2026-07-07 21:05:40 +0000

    v2

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie,Review
 
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['title']
+    search_fields = ['title','description']
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
